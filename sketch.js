6b5e0e1a4443191df047dd93c6ecfb85b78fc867
Simon-Chase <kingkhan007ka@gmail.com>
2017-03-28 16:44:36 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,15 @@
 function setup() {
+  angleMode("degrees");
   createCanvas(600, 400);
 }
 
 function draw() {
-  
+  background(255);
+  fill(0);
+  rectMode(CENTER);
+  noStroke();
+  push();
+  rotate(rotationZ);
+  rect(300, 200, 300, 200);
+  pop();
 }
