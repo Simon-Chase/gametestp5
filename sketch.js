ca00b8c8d8a2e922d59c3fd0a06d980b50504779
Simon-Chase <kingkhan007ka@gmail.com>
2017-04-01 13:33:39 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -6,5 +6,5 @@ function draw() {
   background(255);
   fill(0);
   noStroke();
-  ellipse(300 + cos(-rotationX)*200, 300 + sin(-rotationY)*200, 50, 50);
+  ellipse(300 + cos(90-rotationX)*200, 300 + sin(90-rotationY)*200, 50, 50);
 }
