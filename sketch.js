6d6dd2a5d6c186f663586a535ac40d676f831632
Simon-Chase <kingkhan007ka@gmail.com>
2017-04-01 12:56:58 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,13 +5,13 @@ function setup() {
 
 function draw() {
     background(255);
-    textSize(200);
+    textSize(50);
     textAlign(CENTER);
     push();
     translate(300, 300);
     rotate(-rotationX);
     fill(0);
     noStroke();
-    text('^', 0, 0);
+    text(rotationX, 0, 0);
     pop();
 }
